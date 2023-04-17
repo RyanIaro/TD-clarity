05e962d9ac1bae26777178c5500a1aff00c9fa7b
RyanIaro <hei.ryan.3@gmail.com>
2023-04-17 16:43:05 +0300

    first commit

---
+++ src/test/java/com/example/Mozart/MozartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mozart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
